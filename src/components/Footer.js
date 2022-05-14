b46b2f057641217de3a26a925dc80e5ff32efff1
Prince0047 <sonubhadana838@gmail.com>
2022-05-14 16:17:43 +0530

    udate resume

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,37 +1,37 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import {
   AiFillGithub,
   AiOutlineTwitter,
   AiFillInstagram,
-} from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
+} from 'react-icons/ai';
+import { FaLinkedinIn } from 'react-icons/fa';
 
 function Footer() {
   let date = new Date();
   let year = date.getFullYear();
   return (
-    <Container fluid className="footer">
+    <Container fluid className='footer'>
       <Row>
-        <Col md="4" className="footer-copywright">
+        <Col md='4' className='footer-copywright'>
           <h3>Designed and Developed by Soumyajit Behera</h3>
         </Col>
-        <Col md="4" className="footer-copywright">
+        <Col md='4' className='footer-copywright'>
           <h3>Copyright © {year} SB</h3>
         </Col>
-        <Col md="4" className="footer-body">
-          <ul className="footer-icons">
-            <li className="social-icons">
+        <Col md='4' className='footer-body'>
+          <ul className='footer-icons'>
+            <li className='social-icons'>
               <a
-                href="https://github.com/soumyajit4419"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
+                href='https://github.com/Prince0047'
+                style={{ color: 'white' }}
+                target='_blank'
+                rel='noopener noreferrer'
               >
                 <AiFillGithub />
               </a>
             </li>
-            <li className="social-icons">
+            {/* <li className="social-icons">
               <a
                 href="https://twitter.com/Soumyajit4419"
                 style={{ color: "white" }}
@@ -40,23 +40,23 @@ function Footer() {
               >
                 <AiOutlineTwitter />
               </a>
-            </li>
-            <li className="social-icons">
+            </li> */}
+            <li className='social-icons'>
               <a
-                href="https://www.linkedin.com/in/soumyajit4419/"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
+                href='https://www.linkedin.com/in/sonu-bhadana/'
+                style={{ color: 'white' }}
+                target='_blank'
+                rel='noopener noreferrer'
               >
                 <FaLinkedinIn />
               </a>
             </li>
-            <li className="social-icons">
+            <li className='social-icons'>
               <a
-                href="https://www.instagram.com/s.o.u.m.y.a_j.i.t/"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
+                href='https://www.instagram.com/bhadana0047/'
+                style={{ color: 'white' }}
+                target='_blank'
+                rel='noopener noreferrer'
               >
                 <AiFillInstagram />
               </a>
